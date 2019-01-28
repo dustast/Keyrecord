b639ba9343458d6b86acb31978beaa44b3c716e5
Dust <cragyn@gmail.com>
2019-01-27 23:16:28 -0500

    Updated Helper header

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main ()
     MSG Msg;
     //message is called from the windows thread message queue to be translated
     // message loop is created  always running and hidden
-    // since handle is null windows does nothing with the dispatch message
+    // since handle is null windows does nothing with the disp message
     while(GetMessage (&Msg, NULL, 0, 0))
     {
         TranslateMessage(&Msg);
